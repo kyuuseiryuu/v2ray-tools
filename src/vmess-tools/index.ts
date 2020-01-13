8bfe61291fd7428e49e07b4575209b9b0573d3f3
kyuuseiryuu <kyuuseiryuu@gmail.com>
2020-01-13 21:09:21 +0800

    not throw error

--- src/vmess-tools/index.ts
+++ src/vmess-tools/index.ts
@@ -1,15 +1,15 @@
 import {VMessV2} from "./types";
 
 const v1ToV2Mapper = {
-  remarks:    'ps',
-  obfsParam:  'host',
-  obfs:       'net',
+  remarks: 'ps',
+  obfsParam: 'host',
+  obfs: 'net',
 };
 
 const v2ToV1Mapper = {
-  ps:     'remarks',
-  host:   'obfsParam',
-  net:   'obfs',
+  ps: 'remarks',
+  host: 'obfsParam',
+  net: 'obfs',
 };
 
 const v1Converter = {
@@ -25,6 +25,12 @@ const tryToParseJson = (str: string): any => {
   } catch (e) {}
 };
 
+const tryAToB = (encoded: string) => {
+  try {
+    return atob(encoded);
+  } catch (e) {}
+};
+
 export const isVMessLink = (link: string): boolean => /^vmess:\/\//i.test(link);
 
 export const isVMessLinkV1 = (link: string): boolean => {
@@ -34,15 +40,15 @@ export const isVMessLinkV1 = (link: string): boolean => {
 export const isVMessLinkV2 = (link: string): boolean => {
   return isVMessLink(link)
     && !link.includes('?')
-    && tryToParseJson(atob(link.replace(/^vmess:\/\//i, '')));
+    && tryToParseJson(tryAToB(link.replace(/^vmess:\/\//i, '')));
 };
 
-export const parseV1Link = (v1Link: string): VMessV2 => {
-  if (!isVMessLinkV1(v1Link)) {
-    throw new Error('不是 v1 版本的 VMess 链接');
-  }
+export const parseV1Link = (v1Link: string): VMessV2 | undefined => {
+  if (!isVMessLinkV1(v1Link)) return;
   const [main, searchStr] = v1Link.replace(/^vmess:\/\//i, '').split('?');
-  const [type, id, add, port] = atob(main).split(/[@:]/);
+  const s = tryAToB(main);
+  if (!s) return;
+  const [type, id, add, port] = s.split(/[@:]/);
   const search = {};
   const searchParams = new URLSearchParams(searchStr);
   searchParams.forEach((value, key) => {
@@ -59,15 +65,15 @@ export const parseV1Link = (v1Link: string): VMessV2 => {
   };
 };
 
-export const parseV2Link = (link: string): VMessV2 => {
-  if (!isVMessLinkV2(link)) {
-    throw new Error('不是 v2 版本的 VMess 链接');
-  }
-  return JSON.parse(atob(link.replace(/^vmess:\/\//i, '')));
+export const parseV2Link = (link: string): VMessV2 | undefined => {
+  if (!isVMessLinkV2(link)) return;
+  const s = atob(link.replace(/^vmess:\/\//i, ''));
+  if (!s) return;
+  return JSON.parse(s);
 };
 
-export const toV1Link = (link: string) => {
-  if (!isVMessLink(link)) throw new Error('不是合法的 VMess 链接');
+export const toV1Link = (link: string): string => {
+  if (!isVMessLink(link)) return '';
   if (isVMessLinkV1(link)) return link;
   const { v, type, id, port, add, ...others} = parseV2Link(link);
   const searchParams = new URLSearchParams();
@@ -79,8 +85,8 @@ export const toV1Link = (link: string) => {
   return `vmess://${btoa(`${type}:${id}@${add}:${port}`)}?${decodeURIComponent(searchParams.toString())}`;
 };
 
-export const toV2Link = (link: string) => {
-  if (!isVMessLink(link)) throw new Error('不是合法的 VMess 链接');
+export const toV2Link = (link: string): string => {
+  if (!isVMessLink(link)) return '';
   if (isVMessLinkV2(link)) return link;
   const v2 = parseV1Link(link);
   return `vmess://${btoa(JSON.stringify({ ...v2, v: '2' }))}`;
