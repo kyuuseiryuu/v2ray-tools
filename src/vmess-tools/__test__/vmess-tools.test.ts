b98a835b68e1b848cd1000d7129262381f455c3f
kyuuseiryuu <kyuuseiryuu@gmail.com>
2020-01-13 14:04:57 +0800

    update test case

--- src/vmess-tools/__test__/vmess-tools.test.ts
+++ src/vmess-tools/__test__/vmess-tools.test.ts
@@ -17,29 +17,29 @@ describe('test vmess-tools', () => {
     expect(v2).toHaveProperty('host');
     expect(v2).toHaveProperty('type');
   });
-  test('test toV2Link', () => {
+  test('test v1 -> v2', () => {
     const l = toV2Link(v1Link);
     expect(l).toBeTruthy();
     expect(toV2Link(v2Link)).toEqual(v2Link);
   });
-  test('test toV1Link', () => {
+  test('test v2 -> v1', () => {
     const l = toV1Link(v2Link);
     expect(toV1Link(v1Link)).toEqual(v1Link);
     expect(isVMessLink(l)).toBeTruthy();
   });
-  test('test v1 多次转换', () => {
+  test('test v1 -> v2 -> v1 (1)', () => {
     const l1 = 'vmess://YXV0bzo2ZjU4NjE0Ny1hZWU5LTQ5ZjUtYjI1ZC1hMjVmMmQ5MmI5ODRANDcuMjQ0LjIzMy4xMjY6NDQz?remarks=hk-fake&obfsParam=networkservice.qq.com&path=/websocket&obfs=websocket&tls=1&peer=&allowInsecure=1&cert=';
     const l2 = toV2Link(l1);
     const l1_1 = toV1Link(l2);
     expect(l1_1).toEqual(l1);
   });
-  test('test v1 多次转换 (含中文）', () => {
+  test('test v1 -> v2 -> v1 (2)', () => {
     const l1 = 'vmess://bm9uZTo0NjFiNzM3Mi1iMjg2LTRkZTgtODJlNi0yMWYxMzAxMzFmMDRAbHMuaGt0bHMubWw6NjAxMjA=?remarks=%E9%82%80%E8%AF%B7%E7%A0%81NEUK.%E8%B0%B7%E6%AD%8C%E6%90%9CNEUK%E5%85%AC%E7%9B%8A%E5%8A%A0%E9%80%9F&cert=';
     const l2 = toV2Link(l1);
     const l1_1 = toV1Link(l2);
     expect(l1_1).toEqual(l1);
   });
-  test('test v2 多次转换', () => {
+  test('test v2 -> v1 -> v2', () => {
     const l1 = toV1Link(v2Link);
     const l2 = toV2Link(l1);
     const a = parseV2Link(v2Link);
